90efbbac9a55108a1b436452d17d83a657b1dc1b
alone_wolf <alonewollfffff@gmail.com>
2026-01-20 14:19:29 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,11 +5,11 @@ import requests
 import io
 
 # Load model from public URL
-model_url = "https://drive.google.com/file/d/1SMHmW9z4YBAxic80_jokMqIatRGaQqMf/view?usp=sharing"
+model_url = "https://drive.usercontent.google.com/download?id=1SMHmW9z4YBAxic80_jokMqIatRGaQqMf&export=download&authuser=2&confirm=t&uuid=09afc998-0fcf-47a1-9316-955ecf47c8e6&at=ANTm3czO9G_znnHgiC4MaIj6bKOE:1768898932855"
 response = requests.get(model_url)
 model = pickle.load(io.BytesIO(response.content))
 
-vectorizer_url = "https://drive.google.com/file/d/1YLRGNiBU_jCI1xT10T6ie0BbBXGn9k6B/view?usp=sharing"
+vectorizer_url = "https://drive.usercontent.google.com/download?id=1YLRGNiBU_jCI1xT10T6ie0BbBXGn9k6B&export=download&authuser=2&confirm=t&uuid=8bd5480e-5b6b-4ad0-b549-3b53f0980525&at=ANTm3cwFhr0SJeSdCLo0SF8uYYb7:1768898907043"
 response = requests.get(vectorizer_url)
 tfidf = pickle.load(io.BytesIO(response.content))
 
@@ -62,6 +62,7 @@ if st.button('Predict..'):
         st.header('Spam')
     else:
         st.header('Not Spam')
+
 
 
 
